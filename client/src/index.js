37df7cda20a7828f1d0fcc0648e51385a9c64656
kgh919 <jin1991919@hotmail.com>
2020-01-10 15:44:22 +0800

    add post

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 
+serviceWorker.unregister();
